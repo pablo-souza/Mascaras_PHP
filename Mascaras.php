3d64c2c2ec4c38b0e8624f0f4388c7a01d11d22f
pablo-souza <47615568+pablo-souza@users.noreply.github.com>
2020-01-02 10:14:29 -0300

    Add files via upload

--- Mascaras.php
+++ Mascaras.php
@@ -3,9 +3,9 @@ class Mascaras{
     private $numero;
     private $mascara;
     function __construct($numero=null,$mascara=null) {
-        $this->numero = $numero;
+        $this->numero = preg_replace("/[^0-9]/","", $numero);
         if($mascara!=null && 
-        strlen(preg_replace("/[^#]/","",$mascara))==strlen($numero)){
+        strlen(preg_replace("/[^#]/","",$mascara))==strlen($this->numero)){
             $this->mascara = $mascara;
         } 
     }
@@ -78,3 +78,25 @@ class Mascaras{
         
     }
 }
+//Exemplos
+//Mascara personalizada:
+$mascara = new Mascaras("14.99","R$ ##,##");
+echo $mascara->gerarMasc()."\n";
+//Mascara CPF:
+$mascara = new Mascaras();
+echo $mascara->mascCPF("11111111111")."\n";
+//Mascara CNPJ:
+$mascara = new Mascaras();
+echo $mascara->mascCNPJ("03352758000187")."\n";
+//Mascara CEP:
+$mascara = new Mascaras();
+echo $mascara->mascCEP("11100000")."\n";
+//Mascara Telefone:
+$mascara = new Mascaras();
+echo "Celular com DD: ".$mascara->mascTelefone("99999999999")."\n";
+$mascara = new Mascaras();
+echo "Celular sem DD: ".$mascara->mascTelefone("999999999")."\n";
+$mascara = new Mascaras();
+echo "Fixo com DD: ".$mascara->mascTelefone("9911111111")."\n";
+$mascara = new Mascaras();
+echo "Fixo sem DD: ".$mascara->mascTelefone("11111111")."\n";
